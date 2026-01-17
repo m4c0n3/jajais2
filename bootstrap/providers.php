7878a8c5881605772d662db7057e49fad2eb99ef
marcel <marcel.pajtas@gmail.com>
2026-01-17 02:56:30 +0100

    CP-0009.1: remove duplicate Filament panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\JajaisadminPanelProvider::class,
     App\Providers\ModuleSystemServiceProvider::class,
     Modules\Agent\Providers\AgentServiceProvider::class,
 ];
